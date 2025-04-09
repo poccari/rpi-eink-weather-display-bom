5eebc8220633f703efe2337ec5daa9db71f55a35
AndyLeonard <andrew.leonard@resonatesystems.com.au>
2025-04-09 21:16:25 +0930

    adding testing argparse

--- render_webpage/mainFlask.py
+++ render_webpage/mainFlask.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from collector import Collector
 import arrow
+import argparse
 
 app = Flask(__name__)
 lat = -34.92866000
@@ -19,10 +20,10 @@ testPath = os.path.join(os.path.dirname(__file__), 'testFixtures', 'mockWeatherD
 
 @app.route('/')
 def index():
+    print(f"Testing mode: {testing}")
     mycol = Collector(lat, long,test=testing, test_json=testPath)
     mycol.async_update()
-    last_sync = "24/10/2023 12:00:00 AM"
-    battery_status = "89%"
+    battery_status = "89"
     current_data = mycol.observations_data['data']
     hourly_forecast = mycol.hourly_forecasts_data['data'][0:10]
     daily_forecast = mycol.daily_forecasts_data['data'][0:6]
@@ -40,16 +41,30 @@ def index():
                            weather_warnings=weather_warnings,
                            locations_data=locations_data)
 
-# @app.route('/screenshot')
-# def screenshot():
-#     options = Options()
-#     options.add_argument('--headless')
-#     options.add_argument('--window-size=600x448')
-#     driver = webdriver.Chrome(options=options)
-#     driver.get('http://127.0.0.1:5000/')
-#     driver.save_screenshot('weather.png')
-#     driver.quit()
-#     return "Screenshot saved as weather.png"
+@app.route('/screenshot')
+def screenshot():
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--window-size=600x448')
+    driver = webdriver.Chrome(options=options)
+    driver.get('http://127.0.0.1:5000/')
+    driver.save_screenshot('weather.png')
+    driver.quit()
+    return "Screenshot saved as weather.png"
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Runs a flask application which grabs and renders weather data from BOM API")
+    parser.add_argument("--testing", action="store_true", help="Enable test mode doesn't pull from BOM and uses stored data in json.")
+    return parser.parse_args()
+
+def main():
+    global testing
+    args = parse_arguments()
+    testing = args.testing
+    print(f"Testing mode: {testing}")
+    app.run(debug=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    main()
+
