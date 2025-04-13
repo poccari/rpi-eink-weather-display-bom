2712c29f5caf87ca9337fdb069b0fa3af0b50a28
Andy Leonard <git@alias.eonard.com>
2025-04-13 22:36:01 +0930

    initial code for image flasher

--- image_flasher/image_flasher.py
+++ image_flasher/image_flasher.py
@@ -5,7 +5,48 @@ Flashes screenshot to e-ink display
 
 
 import requests
+from PIL import Image
+from inky.auto import auto
 
-response = requests.get("http://127.0.0.1/screenshot")
-with open("weather_screenshot.png", "wb") as f:
-    f.write(response.content)
+
+def get_screenshot(image_filepath):
+    response = requests.get("http://127.0.0.1/screenshot")
+    with open(image_filepath, "wb") as f:
+        f.write(response.content)
+
+def set_up_display():
+    try:
+        inky_display = auto(ask_user=True, verbose=True)
+    except TypeError:
+        raise TypeError("You need to update the Inky library to >= v1.1.0")
+
+    try:
+        inky_display.set_border(inky_display.BLACK)
+    except NotImplementedError:
+        pass
+    return inky_display
+
+def flash_display(image_path,inky_display):
+    if inky_display.resolution in ((600, 448),):
+        #not sure if this is necessary as we control the image ,but to be sure?
+        img = Image.open(image_path)
+        img = img.resize(inky_display.resolution)
+    # Display the weather dash
+    img = Image.open(image_path)
+    inky_display.set_image(img)
+    inky_display.show()
+
+
+if __name__ == "__main__":
+    """
+    * get screenshot (pass battery % as argument?)
+    * get display
+    * flash image
+    *shut down if required
+        *shutdown process involves:
+            * getting next wake time from config?
+            * setting next wake time in pijuice
+            * if SSH session is there, don't send poweroff assume user is connected and leave it up to user
+            * else - send poweroff signal to pijuice and also shut down the pi
+    """
+    pass
