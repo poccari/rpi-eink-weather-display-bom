914fb74e4c6da7f56ffb512f61cba42bb67754a7
AndyLeonard <andrew.leonard@resonatesystems.com.au>
2025-05-20 15:51:01 +0930

    updates to manual shutdown code due to refactor

--- image_flasher/manual_shutdown.py
+++ image_flasher/manual_shutdown.py
@@ -1,23 +1,16 @@
 """
 manually shuts down the system but allows wakups
-#TODO: read from config file
 #TODO: use argparse to pass in flags for no wakeups etc
 """
 
-import logging
-from image_flasher import shutdown, get_pijuice, enable_wakeups
+from image_flasher import image_flasher
 import os
 import json
 
 
-mylogger = logging.getLogger(__name__)
-
 if __name__ == '__main__':
-    logging.info('Running shutdown.py')
-    piJuice_addr = 0x14
-    pj = get_pijuice(piJuice_addr)
-    config_fp = os.path.abspath(os.path.join(os.path.dirname(__file__),"..","config.json"))
-    with open(config_fp, 'r') as f:
+    with open(os.path.abspath(os.path.join(os.path.dirname(__file__),"..","config.json")), 'r') as f:
         config = json.load(f)
-    enable_wakeups(pj,config['wakeup_hours'], config['timezone'])
-    shutdown(pj,passedLogger=mylogger)
+    flasher_Obj = image_flasher(config,pijuice_addr=0x14)
+    flasher_Obj.shutdown()
+   
